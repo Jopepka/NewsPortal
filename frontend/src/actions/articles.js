528a4d49cd7122839587263f3f7c8b426367eeab
Александр Вороненко <mr_fisher2000@mail.ru>
2023-10-15 20:12:53 +0300

    fixed working with env

--- frontend/src/actions/articles.js
+++ frontend/src/actions/articles.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { setArticles, setCurrentArticles, setIsFetching } from '../reducers/articlesReducer';
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const getArticles = (category = 'general') => {
     return async (dispatch) => {
